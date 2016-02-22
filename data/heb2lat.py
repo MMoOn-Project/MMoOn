2546803fbc7cb77ad54c2f8717486db1af1c61ac
Natanael Arndt <arndtn@gmail.com>
2016-02-22 18:07:47 +0100

    Updated heb2lat

--- data/heb2lat.py
+++ data/heb2lat.py
@@ -2,8 +2,9 @@
 
 
 import string
+import unicodedata
 
-alephbetOne = {
+consonants = {
     "א": "a",   # e
     "ב": "b",
     "ג": "g",
@@ -13,7 +14,7 @@ alephbetOne = {
     "ז": "z",
     "ח": "x",  # h     x
     "ט": "t",   # th
-    "י": "y",
+    "י": "j",
     "כ": "k",
     "ך": "k",
     "ל": "l",
@@ -22,7 +23,7 @@ alephbetOne = {
     "נ": "n",
     "ן": "n",
     "ס": "s",   # sz
-    "ע": "e",   # a
+    "ע": "y",   # a
     "פ": "p",
     "ף": "p",
     "צ": "c",  #       c
@@ -30,53 +31,94 @@ alephbetOne = {
     "ק": "q",
     "ר": "r",
     "שׁ": "v", #       v
-    "שׂ": "u",  # s    u
+    "שׂ": "v",  # s    u
     "ש": "v",  #       v
     "ת": "f"   # t     f
 }
 
-alephbetTwo = {
-    "א": "a",   # e
-    "ב": "b",
-    "ג": "g",
-    "ד": "d",
-    "ה": "h",   # ch
-    "ו": "w",
-    "ז": "z",
-    "ח": "ch",  # h     x
-    "ט": "t",   # th
-    "י": "y",
-    "כ": "k",
-    "ך": "k",
-    "ל": "l",
-    "מ": "m",
-    "ם": "m",
-    "נ": "n",
-    "ן": "n",
-    "ס": "s",   # sz
-    "ע": "e",   # a
-    "פ": "p",
-    "ף": "p",
-    "צ": "ts",  #       c
-    "ץ": "ts",  #       c
-    "ק": "q",
-    "ר": "r",
-    "שׁ": "sh", #       v
-    "שׂ": "sz",  # s    u
-    #"ש": "sh",  #       v
-    "ת": "th"   # t     f
+vowel = {
+    "\u05b0": "m",
+    "\u05b4": "i",
+    "\u05b5": "e",
+    "\u05b6": "n",
+    "\u05b1": "n",
+    "\u05b2": "a",
+    "\u05b7": "a",
+    "\u05b8": "t",
+    "\u05b9": "o",
+    "\u05bb": "u",
 }
-#"ש": "sh",  #       v
+
+alephbetOne = consonants
+
+class glyph:
+    consonant = '_'     # a-z
+    position = 0        # 0-2,89
+    vowel = '-'         # a-z
+    length = None       # 3-7
+    stress = False      # (True, False) "."
+    def __repr__(self):
+        if (self.length == None):
+            length = ""
+        else:
+            length = self.length
+        if (self.stress):
+            stress = "."
+        else:
+            stress = ""
+        if (self.position == 0):
+            position = ""
+        else:
+            position = str(self.position)
+        return self.consonant + position + self.vowel + length + stress
 
 
-def heb2lat(text, singleLetters=False):
-    if (singleLetters):
-        alephbet = alephbetOne
-    else:
-        alephbet = alephbetTwo
-    for k,v in alephbet.items():
+def heb2latComp(text):
+    for k,v in consonants.items():
         text = text.replace(k,v)
     # shin sin and without dot has to be treated separately because of the order of iterating the alephbet list
     text = text.replace("ש", "sh")
 
     return text
+
+def heb2lat(text):
+    text = unicodedata.normalize("NFKD", text)
+    # iterate through text
+    g = None
+    for c in text:
+        #  find placeholder Kringel replace by "_"
+        name = unicodedata.name(c).split(" ")
+        category = unicodedata.category(c)
+        if category != 'Mn':
+            if (g != None):
+                print (g)
+            g = glyph()
+        if (name[0] == "HEBREW"):
+            #  replace letter according to alephbet, caution for shin
+            #   check if sofit letter add 9, else 0
+            # get punctuation
+            #  if no punctuation write "-"
+            #  find punctuation
+            if (name[1] == "LETTER"):
+                #print(unicodedata.category(c), unicodedata.name(c),  end="\n")
+                if c in consonants:
+                    g.consonant = consonants[c]
+                if (name[2] == "FINAL"):
+                    g.position = 9
+            elif (name[1] == "POINT"):
+                #print(unicodedata.category(c), unicodedata.name(c),  end="\n")
+                if c in vowel:
+                    g.vowel = vowel[c]
+                elif c == "\u05bc":
+                    g.position = 8
+    print (g)
+
+    #for k,v in consonants.items():
+    #    text = text.replace(k,v)
+    # shin sin and without dot has to be treated separately because of the order of iterating the alephbet list
+    # text = text.replace("ש", "sh")
+
+    return text
+
+
+heb2lat("hallo äöü ÄÖÜ HALLO לכד אָכּלֻץ אָאּגּהּ ְ ◌");
