53530850bcdb57fafdbaf9a0ea44d281c24fc2f5
Natanael Arndt <arndtn@gmail.com>
2016-03-09 15:25:22 +0100

    Improve comments in the example code

--- data/datamodel_example.py
+++ data/datamodel_example.py
@@ -29,14 +29,18 @@ def main(args=sys.argv[1:]):
         else:
             assert False, "unhandled option"
 
+    # TODO maybe we should use the latin code for the strings especially the  transfixes, because of the dagesh issue
+    #
+    # Lexeme takes the binjan as second argument:
+    #   e.g. datamodel.schema.Barkali_piel_1
+    # The third argument is the word class:
+    #   e.g. datamodel.schema.Verb
+    # For th affix/transfix its relation to a morpheme has to be specified:
+    #   e.g. mmoon.ABSINF, mmoon.KAFINF, or mmoon.BETINF, …
+    # The wordform has to be provided with the relations to the root and the affix it consists of
+
     lexeme = Lexeme("דִּבֵּר", datamodel.schema.Barkali_piel_1, datamodel.schema.Verb)
     root = Root("דבר")
-
-    # TODO maybe we should use the latin code for the transfixes, because of the dagesh
-    # mmoon.ABSINF
-    # mmoon.KAFINF
-    # mmoon.BETINF
-    # …
     affix = Affix("◌ִּ◌ֵּ◌", datamodel.inventory.ABSINF)
     wordform = Wordform("דְּבֵּר", root, affix)
     lexeme.addWordform(wordform)
